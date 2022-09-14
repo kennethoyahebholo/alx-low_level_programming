198a83e7c384b7a609958a729e0af582abcb2035
ken <oyahebholokennethizosea@gmail.com>
2022-09-13 21:45:02 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print the alphabet in lowercase.
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
