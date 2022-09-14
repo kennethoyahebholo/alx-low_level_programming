c7f08ecb09fac21454417b6d2aeab37914e32fc1
ken <oyahebholokennethizosea@gmail.com>
2022-09-13 21:27:33 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n")
+
+											return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Task 1: Write a program that prints _putchar, followed by a new line.
+
+Task 2: Write a function that prints the alphabet, in lowercase, followed by a new line.
