1fa489007b95ffc67f45b9b022a25ca67a01be64
ken <oyahebholokennethizosea@gmail.com>
2022-09-15 05:18:23 -0700

    updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,14 +7,16 @@
  * Return: returns 1 if c is a digit between 0 and 9 , 0 if not
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
+
 {
-	if ((c >= 0) && (c <= 9))
-	{
+
+	if ((c >= 48) && (c <= 57))
+
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+
+
+	return (0);
+
 }
