8c6d76b331611a89ff0a362efec1604d98462f92
ken <oyahebholokennethizosea@gmail.com>
2022-09-16 00:18:32 -0700

    The “Fizz-Buzz test”

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,36 +9,27 @@
  * 3 & 5 multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
 		else
-		{
-			prinf(" %d", i);
-		}
+			printf("%d ", i);
 	}
 	printf("\n");
-
 	return (0);
 }
