a61953ac9b7fc36e4d65510cafd1e8716e2770ee
ken <oyahebholokennethizosea@gmail.com>
2022-09-15 23:35:38 -0700

    The “Fizz-Buzz test”

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,25 +20,15 @@ int main(void)
 	for (i = 0; i <= 100; i++)
 	{
 		if (i == 100)
-		{
 			printf("%s", b);
-		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
 			printf("%s", fb);
-		}
 		else if (i % 3 == 0)
-		{
 			printf("%s", f);
-		}
 		else if (i % 5 == 0)
-		{
 			printf("%s", b);
-		}
 		else
-		{
 			prinf("%d", i);
-		}
 	}
 	printf("\n");
 	return (0);
