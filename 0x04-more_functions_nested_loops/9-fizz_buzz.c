a824aaf2fe0a121b2c9a6c601d9093bfef1715ca
ken <oyahebholokennethizosea@gmail.com>
2022-09-15 23:26:34 -0700

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,11 +23,11 @@ int main(void)
 		{
 			printf("%s", fb);
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("%s", f);
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("%s", b);
 		}
